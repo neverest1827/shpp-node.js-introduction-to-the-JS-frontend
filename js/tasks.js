81fde157b05ce7cc683fc420c64f70dba7670934
Vitalii Fetisov <neverest1827@gmail.com>
2023-10-17 06:20:52 +0300

    add tasks 13-18

--- js/tasks.js
+++ js/tasks.js
@@ -1,11 +1,10 @@
+/* Task1 */
 const rect = document.getElementById('rect');
 
 const buttonNone = document.querySelector('.button__box-none');
 const buttonRemove = document.querySelector('.button__box-remove');
 const buttonHidden = document.querySelector('.button__box-hiden');
 const switchButton = document.querySelector('.button__box-swich');
-
-/* Task1 */
 
 buttonNone.addEventListener('click', function(){
     rect.style.display = 'none';
@@ -155,3 +154,145 @@ if ("geolocation" in navigator) {
     console.log("Geo is not support");
 }
 
+/* Task13 */
+
+const local = document.getElementById('local');
+const coockies = document.getElementById('coockies');
+const session = document.getElementById('session');
+
+const boxes = document.getElementsByClassName('editable__box');
+const defaultText = "Some text"
+
+window.addEventListener('load', function(){
+    let localText = this.localStorage.getItem('local') || defaultText;
+    let sessionText = this.sessionStorage.getItem('session') || defaultText;
+    let coockiesText = getCookie('coockies') || defaultText;
+    local.innerHTML = localText;
+    session.innerHTML = sessionText;
+    coockies.innerHTML = coockiesText;
+});
+
+function getCookie(cname) {
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieElements = decodedCookie.split(';');
+    for (let element of cookieElements) {
+        let [key, value] = element.trim().split("=");
+        if (key === cname) {
+            return value;
+        }
+    }
+    return null;
+}
+
+for(let box of boxes){
+    box.addEventListener('input', function(event){
+        let changedText = event.target.innerHTML;
+        let elementId = event.target.id;
+        console.log(elementId);
+        switch (elementId) {
+            case 'local':
+                localStorage.setItem(elementId, changedText);
+                break;
+        
+            case 'coockies':
+                const date = new Date();
+                date.setTime(date.getTime() + 1000) // 1minute
+                let expires = "expires="+ date.toUTCString();
+                document.cookie = encodeURIComponent(`${elementId}=${changedText};${expires};path=/`);
+                console.log(decodeURIComponent(document.cookie));
+                break;
+
+            case 'session':
+                sessionStorage.setItem(elementId, changedText);
+                break;
+
+            default:
+                break;
+        }
+    });
+}
+
+/* Task14 */
+
+const linkUp = document.querySelector('.link__up');
+
+window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 500) {
+        linkUp.style.display = "block";
+    } else {
+        linkUp.style.display = "none";
+    }
+});
+  
+
+linkUp.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+});
+
+/* Task15 */
+
+const doobleBoxs = document.getElementsByClassName('double__box');
+
+for(let box of doobleBoxs){
+    box.addEventListener('click', function(event){
+        event.stopPropagation();
+        alert(event.target.id);
+    });
+};
+
+/* Task16 */
+
+const popupBtn = document.querySelector('.popup__btn');
+const popup = document.querySelector('.popup')
+
+popupBtn.addEventListener('click', function(){
+    popup.style.display = 'block';
+    popup.addEventListener('click', function(){
+        popup.style.display = 'none';
+    })
+});
+
+/* Task17 */
+
+const forma = document.querySelector('form');
+
+forma.addEventListener('submit', function(event){
+    event.preventDefault();
+})
+
+/* Task18 */
+
+const fileDropArea = document.getElementById('fileDropArea');
+
+['dragenter', 'dragover', 'dragleave','drop'].forEach(eventName => {
+    fileDropArea.addEventListener(eventName, preventDefaults, false);
+});
+
+function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+['dragenter', 'dragover'].forEach(eventName => {
+    fileDropArea.addEventListener(eventName, highlight, false);
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+    fileDropArea.addEventListener(eventName, unhighlight, false);
+});
+
+function highlight() {
+    fileDropArea.classList.add('hover');
+}
+
+function unhighlight() {
+    fileDropArea.classList.remove('hover');
+}
+
+fileDropArea.addEventListener('drop', function(e){
+    fileDropArea.classList.add('drop')
+});
+
+fileDropArea.addEventListener('change', function(){
+    fileDropArea.classList.add('drop')
+})
